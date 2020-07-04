676a6ddcde94156a3ceb04adc84bfeb51943517b
cannot-fly-pig <document.sabakan@gmail.com>
2020-07-05 08:25:06 +0900

    make header & footer

---
+++ components/Footer.vue
@@ -0,0 +1,87 @@
+<template>
+	<footer>
+		<div class="logo-wrap">
+			<div class="logo-ruby">オンライン</div>
+			<div class="logo">文化祭</div>
+		</div>
+
+		<div class="sns-wrap">
+			<div>Follow Us!</div>
+			<div class="sns">
+				<a href="https://twitter.com/Nada_festival?ref_src=twsrc%5Egoogle%7Ctwcamp%5Eserp%7Ctwgr%5Eauthor">
+					<img src="" alt="twitter" class="sns-img">
+				</a>
+				<a href="https://www.facebook.com/NadaSchoolFestival/">
+					<img src="" alt="facebook" class="sns-img">
+				</a>
+				<a href="https://www.instagram.com/nada_schoolfestival/">
+					<img src="" alt="instagram" class="sns-img">
+				</a>
+			</div>
+		</div>
+
+		<div class="privacyPolicy">
+			<div>privacy policy</div>
+			<div class="spacer"></div>
+			<div>Design Team @ 2020</div>
+		</div>
+	</footer>
+</template>
+
+<style>
+footer {
+	background-color: #333333;
+	color: #d6d6d6;
+	height: 15vh;
+	width: 100vw;
+	bottom: 0;
+	left: 0;
+	display: flex;
+	flex-direction: row;
+	justify-content: space-around;
+	align-items: center;
+	padding-left: 10%;
+	padding-right: 10%;
+}
+
+footer > .logo-wrap {
+	text-align: center;
+}
+
+footer > * > .logo-ruby {
+	font-size: 0.8em;
+}
+
+footer > * > .logo {
+	font-size: 1.5em;
+}
+
+.sns-wrap {
+	display: flex;
+	flex-direction: column;
+	justify-content: center;
+	text-align: center;
+}
+
+.sns {
+	display: flex;
+	flex-direction: row;
+	justify-content: space-around;
+}
+
+.sns-img {
+	height: 8vh;
+	width: 8vh;
+	border: solid white 1px;
+}
+
+.privacyPolicy {
+	display: flex;
+	flex-direction: row;
+	font-size: 0.8em;
+}
+
+.privacyPolicy > .spacer {
+	width: 2em;
+}
+</style>
---
+++ components/Header.vue
@@ -0,0 +1,41 @@
+<template>
+<header>
+	<div class="logo-wrap">
+		<div class="logo-ruby">オンライン</div>
+		<div class="logo">文化祭</div>
+	</div>
+	<div class="link">About</div>
+	<div class="link">サークル</div>
+	<div class="link">ステージ</div>
+	<div class="link">装飾</div>
+</header>
+</template>
+
+<style>
+header {
+	height: 8vh;
+	width: 100vw;
+	position: fixed;
+	top: 0;
+	left: 0;
+	display: flex;
+	flex-direction: row;
+	justify-content: space-around;
+	align-items: center;
+	padding-left: 10%;
+	padding-right: 10%;
+}
+
+header > .logo-wrap {
+	text-align: center;
+}
+
+header > * > .logo {
+	font-size: 1.4em;
+}
+
+header > * > .logo-ruby {
+	font-size: 0.6em;
+}
+
+</style>
