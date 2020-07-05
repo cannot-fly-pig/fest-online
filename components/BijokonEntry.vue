712eed0dd1506377dc3bbdb2f9759a4977eb0174
cannot-fly-pig <document.sabakan@gmail.com>
2020-07-05 18:17:39 +0900

    bijokon

---
+++ components/BijokonEntry.vue
@@ -0,0 +1,54 @@
+<template>
+<div class="entryWrap">
+	<img class="entryImage" :src="entryImage" alt="entryName" v-if="imagePosition == 'left'">
+	<div class="textWrap">
+		<h2 class="entryName">No.{{ entryNumber }} {{ entryName }}</h2>
+		<div class="entryText">{{ entryText }}</div>
+	</div>
+	<img class="entryImage" :src="entryImage" alt="entryName" v-if="imagePosition == 'right'">
+</div>
+</template>
+
+<script>
+export default {
+	props: ["entryNumber", "entryName", "entryText", "entryImage", "imagePosition"],
+	data() {
+		return {
+		}
+	}
+}
+</script>
+
+<style>
+.entryWrap {
+	width: 100vw;
+	min-height: 30vh;
+	display: flex;
+	flex-direction: row;
+	align-items: center;
+	padding: 3%;
+}
+.textWrap {
+	width: 60%;
+	background-color: #F8D1E0;
+	opacity: 0.75;
+}
+
+.entryName {
+	font-size: 0.8rem;
+	margin-top: 5%;
+	margin-left: 5%;
+	margin-right: 5%;
+}
+
+.entryText {
+	font-size: 0.6rem;
+	margin-bottom: 5%;
+	margin-left: 5%;
+	margin-right: 5%;
+}
+
+.entryImage{
+	width: 45vw;
+}
+</style>
